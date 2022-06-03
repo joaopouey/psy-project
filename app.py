a5fdc054e3ce6f71387b5746e43a94c0e5de52b9
joaopouey <jcpouey@gmail.com>
2022-06-03 14:24:53 -0300

    confirmacao

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
+from itertools import count
 import sqlite3
-
 from flask import Flask, session, redirect, render_template, request
 from flask_session import Session
-
 from helpers import login_required
+
 
 
 # Create and execute a Flask app
 app = Flask(__name__)
 
-# Open psi.db and create a cursor
-con = sqlite3.connect('psi.db')
+# Connect to database (psi.db)
+con = sqlite3.connect('psi.db', check_same_thread=False)
 cur = con.cursor()
 
 #Configure Session
@@ -19,7 +19,6 @@ Session(app)
 
 
 @app.route("/")
-@login_required
 def index():
     return render_template("index.html")
 
@@ -30,16 +29,29 @@ def login():
     # Esquecer user_id conectado.
     session.clear()
 
-    # Caso o usuário envie um formulário por GET (clicando em um link ou sendo redirecionado)
+    # Caso o usuário envie um formulário por GET (clicando em um link ou sendo redirecionado).
     if request.method == "GET":
         return render_template("login.html")
     
-    # Caso o usuário envie um formulário via POST (tentando fazer o login)
+    # Caso o usuário envie um formulário via POST (tentando fazer o login).
     else:
-        return ('TODO')
+        # Buscar username no banco de dados.
+        
+        user_search = cur.execute("SELECT id FROM usuarios WHERE nome = ?;", [request.form.get("username")])
+        if len(user_search) != 1:
+            return ('Usuário não encontrado')
+        else:
+            return ('kkkkkkk')
+
+@app.route("/cadastro")
+def cadastro():
+    return render_template("cadastro.html")
+
+
+@app.route("/fechamento")
+def fechamento():
+    return render_template("fechamento.html")
 
 
 
-
-con.close()
 
--- db.py
+++ db.py
@@ -4,8 +4,8 @@ con = sqlite3.connect('psi.db')
 cur = con.cursor()
 
 
-# cur.execute ('''CREATE TABLE usuarios (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, nome TEXT NOT NULL, valor INTEGER NOT NULL)''')
-# cur.execute ('''CREATE TABLE consultas(id INTEGER, dias TEXT, nr_consultas INTEGER, total INTEGER, FOREIGN KEY (id) REFERENCES usuarios(id))''')
+# db.execute ('''CREATE TABLE usuarios (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, nome TEXT NOT NULL, valor INTEGER NOT NULL)''')
+# db.execute ('''CREATE TABLE consultas(id INTEGER, dias TEXT, nr_consultas INTEGER, total INTEGER, FOREIGN KEY (id) REFERENCES usuarios(id))''')
 
 
 con.commit()
